9580c9ec0c71c3d48a511a39def7463a98208a65
manideep <manideepanasuri@gmail.com>
2025-03-14 18:21:37 +0530

    static files

--- Tshirts/settings.py
+++ Tshirts/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
+
+from django.contrib import staticfiles
 from dotenv import load_dotenv
 from datetime import timedelta
 from pathlib import Path
@@ -140,7 +142,7 @@ REST_FRAMEWORK = {
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Default primary key field type
